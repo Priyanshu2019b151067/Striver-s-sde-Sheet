71870f376859007677affc4157b032d558625047
Priyanshu <priyanshukingh@gmail.com>
2022-04-11 14:42:27 +0530

    day 8

---
+++ DAY 8/minimum_platform.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    static bool sortbysec(const pair<int,int> & a, const pair<int,int> &b){
+        return a.second < b.second;
+    }
+    
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int,int>> interval;
+        for(int i = 0; i < n;i++){
+            interval.push_back({start[i],end[i]}); 
+        }
+        sort(interval.begin(),interval.end(),sortbysec);
+        int count = 1;
+        int ending_time = interval[0].second;
+        for(int i = 1;i < interval.size();i++){
+            if(interval[i].first > ending_time){
+                count++;
+                ending_time = interval[i].second;
+            }
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
